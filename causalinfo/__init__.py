650bc63ec7fbc9d466a70146808f511fda6dcc0b
Brett Calcott <brett.calcott@gmail.com>
2015-10-28 15:34:43 +1300

    Bump version.

--- causalinfo/__init__.py
+++ causalinfo/__init__.py
@@ -15,7 +15,7 @@ from .payoff import PayoffMatrix
 
 import equations
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 __title__ = "causalinfo"
 __description__ = "Information Measures on Causal Graphs."
